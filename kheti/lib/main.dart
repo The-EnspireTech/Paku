68e56e25f5a44ec9131945b16b52dedcb1d4e9eb
Niraj <nirajpoudel1818@gmail.com>
2020-11-22 12:26:29 +0545

    Adding weather api

--- kheti/lib/main.dart
+++ kheti/lib/main.dart
@@ -1,13 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoder/geocoder.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
 /// This is the main application widget.
 class MyApp extends StatelessWidget {
-  static const String _title = 'Kheti - The farmers choice';
+  static const String _title = 'Kheti-The farmers choice';
 
   @override
   Widget build(BuildContext context) {
@@ -28,17 +30,22 @@ class MyStatefulWidget extends StatefulWidget {
 
 /// This is the private State class that goes with MyStatefulWidget.
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
   static String message = "";
-  //-----------------------------------------------------------------------------------
-  // Longitude and latitude
+  static String temperature = "";
+  static String pressure = "";
+  static String humidity = "";
+  static String wind = "";
+  static String description = "";
 
   @override
   void initState() {
     super.initState();
-    getCurrentLocation();
+    _getCurrentLocation();
+    print(temperature);
   }
 
-  void getCurrentLocation() async {
+  void _getCurrentLocation() async {
     final position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
 
@@ -46,57 +53,82 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     var addresses =
         await Geocoder.local.findAddressesFromCoordinates(coordinates);
     var first = addresses.first;
+    http.Response response = await http.get(
+        "https://api.openweathermap.org/data/2.5/weather?q=Thada&appid=6829005ae98e929e814158d91327a6db");
+    var results = jsonDecode(response.body);
 
     setState(() {
-      message = "${first.featureName[1]}";
+      message = "${first.locality}";
+      temperature = "${results['main']['temp']}";
+      pressure = "${results['main']['pressure']}";
+      humidity = "${results['main']['humidity']}";
+      wind = "${results['wind']['speed']}";
+      description = "${results['weather'][0]['description']}";
     });
   }
 
-  //---------------------------------------------------------------------------------
+  static TextStyle optionStyle =
+      TextStyle(fontSize: 15, fontWeight: FontWeight.normal);
+  static List<Widget> widgetOptions = <Widget>[
+    Text(
+      'Current Location: ' +
+          message +
+          "\n\n" +
+          'Temperature: ' +
+          temperature +
+          '\n\n' +
+          'Pressure: ' +
+          pressure +
+          '\n\n' +
+          'Humidity: ' +
+          humidity +
+          '\n\n' +
+          'Wind: ' +
+          wind +
+          '\n\n' +
+          'Description: ' +
+          description +
+          '\n\n',
+      style: optionStyle,
+    ),
+    Text(
+      "Hello world",
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Crop Care',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Market',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: News',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 5: Profile',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home"),
+        title: const Text('Home'),
         centerTitle: true,
         backgroundColor: Color.fromRGBO(20, 172, 168, 1),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            Icon(
-              Icons.person_pin_circle,
-              color: Colors.redAccent,
-              size: 40.0,
-            ),
-            Text("\nSandhikharka" + "\n\n"),
-            Icon(
-              Icons.circle,
-              color: Colors.purple,
-              size: 50,
-            ),
-            Text("Temperature"),
-            Icon(
-              Icons.circle,
-              color: Colors.blue,
-              size: 50,
-            ),
-            Text("Precipitation"),
-            Icon(
-              Icons.circle,
-              color: Colors.green,
-              size: 50,
-            ),
-            Text("Humidity"),
-            Icon(
-              Icons.circle,
-              color: Colors.grey,
-              size: 50,
-            ),
-            Text("Wind"),
-          ],
-        ),
+      body: Center(
+        child: widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -110,11 +142,11 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             label: 'Crop Plan',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.medical_services_outlined),
+            icon: Icon(Icons.medical_services_rounded),
             label: 'Crop Care',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.money),
+            icon: Icon(Icons.shopping_basket),
             label: 'Market',
           ),
           BottomNavigationBarItem(
@@ -126,6 +158,9 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             label: 'Profile',
           ),
         ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
       ),
     );
   }
