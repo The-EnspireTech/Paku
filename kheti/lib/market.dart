3fecde3a5a6eba5130bed10c80aab93f2b21e1a9
Niraj <nirajpoudel1818@gmail.com>
2020-12-06 19:01:03 +0545

    updated market

--- kheti/lib/market.dart
+++ kheti/lib/market.dart
@@ -85,11 +85,6 @@ class _WebScraperAppState extends State<Market> {
                                             color: Colors.white,
                                             fontSize: 20)),
                                   ),
-                                  Text(
-                                    post[index],
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 15),
-                                  )
                                 ],
                               ),
                             ),
